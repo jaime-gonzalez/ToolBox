28f7b47ade2ec4985fd372c2d498ba205031f3fe
Jaime Gonzalez <68569736+jaime-gonzalez@users.noreply.github.com>
2021-02-16 10:16:15 -0300

    Update data_analysis_cheatsheet.py

--- data_analysis_cheatsheet.py
+++ data_analysis_cheatsheet.py
@@ -2,8 +2,12 @@
 
 
 #Score R² da Regressão linear entre duas variáveis (x,y)
-
 from sklearn.metrics import r2_score
-
 score = r2_score(df['valor01'],df['valor02'])
+
+#Cálculo dos valores de outliers para uma distribuição normal
+import pandas as pd
+resA1 = df.groupby(['Coluna01','Coluna02'])['Valor'].quantile([0.05]) #Quantil p/ limite inferior até 5% da distribuição normal (2 desvios padrões) 
+resA2 = df.groupby(['Coluna01','Coluna02'])['Valor'].quantile([0.95]) #Quantil p/ limite superior até 95% da distribuição normal (2 desvios padrões)
+res = pd.merge(resA1, resA2, on = ['Coluna01','Coluna02'], how = 'left',suffixes=('_Lim. Inf','_Lim. Sup'))
 
