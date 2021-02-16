872c6e0bfa035c0ba176e8eac8adf1972dad206d
Jaime Gonzalez <68569736+jaime-gonzalez@users.noreply.github.com>
2021-02-16 09:12:43 -0300

    Create data_visualization_cheatsheet.py

---
+++ data_visualization_cheatsheet.py
@@ -0,0 +1,57 @@
+
+
+#Plotar mapa de calor - é preciso eliminar as colunas de texto e manter apenas as de valores
+
+import seaborn as sns
+
+df['coluna01', 'coluna02', 'coluna03', 'valor01', 'valor02', 'valor03', 'valor04']
+df_filtrado = df.drop(columns=['coluna01', 'coluna02', 'coluna03'])
+
+f,ax = plt.subplots(figsize=(20,16))
+sns.heatmap(df_filtrado.corr(),annot=True, 
+            linewidths=.5, fmt='.1f', ax=ax) 
+plt.show()
+
+
+#Gráfico de dispersão 'Scatterplot', com as colunas do valor =_x e valor_y
+
+import matplotlib.pyplot as plt
+
+plt.plot(df['valor01'], df['valor02'], 'o', color='blue');
+plt.title('Título', size = 20)
+plt.xlabel('Rótulo de x',size=16)
+plt.ylabel('Rótulo de y', size=16)
+plt.rcParams['figure.figsize']=(20,16)
+plt.show()
+    
+
+#Plotar histograma com quantidade de intervalos pré definida (sempre colocar apenas a coluna com os valores)
+import matplotlib.pylab as plt
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize']=15,6
+
+df['coluna01', 'coluna02', 'valores']
+
+df_valores = df['valores']
+df_valores.plot(kind='hist', stacked=True,bins=50)
+plt.title('Título do gráfico')
+plt.xlabel('Rótulo do eixo x')
+plt.ylabel('Rótulo do eixo y')
+
+#Gráfico de série temporal com agregação de múltiplos parâmetros da série por valores definidos
+df['data', 'valor']
+fig, ax = plt.subplots(1,1, figsize=(12,10))
+(df
+     .assign(month=lambda df: df.index.month)
+     .groupby('month')['valor'].agg(['mean','std','median','min','max'])
+     .plot(ax=ax, marker='o',linewidth=3)
+ )
+ax.set_xlabel('Mês')
+
+#Plotar resample de uma série temporal com base no período
+df_filtrado = df['data','valor']
+
+df.resample('M').apply([np.mean]).plot()
+plt.title('Título do gráfico')
+plt.ylabel('Rótulo do eixo y')
+plt.xlabel('Rótulo do eixo x')
